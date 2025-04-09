77a38edd03d84a662da64682c9b8a2fbbcd3b340
Hector Soto <hector_17rock@hotmail.com>
2025-04-09 12:34:08 -0400

    Add sorting algorithm implementation

--- sort.h
+++ sort.h
@@ -6,9 +6,9 @@
 /* Structure for a doubly linked list node */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 /* Function prototypes */
